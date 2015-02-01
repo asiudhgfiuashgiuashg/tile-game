9e2fd0b220b2904133443fc62ed4943666c792af
AEthura <arthurofavalon@gmail.com>
2015-01-31 21:02:23 -0500

    I had to push maps for mikey

---
+++ core/src/com/mygdx/game/Map.java
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Project:  theGame-core
+//  File:     Map.java
+//  
+//  Name:     Bhavishya Shah
+//  Email:    bhshah1@my.waketech.edu
+////////////////////////////////////////////////////////////////////////////////
+package com.mygdx.game;
+
+/**
+ * (Insert a comment that briefly describes the purpose of this class definition.)
+ *
+ * <p/> Bugs: (List any known issues or unimplemented features here)
+ * 
+ * @author (Bhavishya Shah)
+ *
+ */
+public class Map
+{
+
+}
