16224c3fb36bc9c817f0b60aba300374b9ce913b
Michael Adkison <madkison3@gatech.edu>
2015-03-07 16:24:03 -0500

    add Item class

---
+++ core/src/com/mygdx/game/Item.java
@@ -0,0 +1,58 @@
+package com.mygdx.game;
+
+public class Item {
+    private String name;
+    private int id;
+
+    private static String classFileLocation;
+
+    private String inventoryImageURI;
+    private String floorImageURI;
+    
+    private int width, height;
+    
+    public int getId() {
+        return id;    
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getInventoryImage()
+    {
+        return inventoryImageURI;
+    }
+ 
+    public String getFloorImage()
+    {
+        return floorImageURI;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    public void setInventoryImage(String URI)
+    {
+        this.inventoryImageURI = URI;
+    }
+    
+    public void setFloorImageURI(String URI)
+    {
+        this.floorImageURI = URI;   
+    }
+    
+}
