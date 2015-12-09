241efa8e022a7feebbf6cb0bc71f8ec56c403fc0
Mike <mikeadkison@gmail.com>
2015-12-09 18:08:16 -0500

    add ChatInputProcessor (not finished)

---
+++ core/src/com/mygdx/game/ChatInputProcessor.java
@@ -0,0 +1,64 @@
+package com.mygdx.game;
+import com.badlogic.gdx.InputProcessor;
+
+/**
+ * 
+ * @author elimonent
+ * handles keypresses for chat
+ */
+public class ChatInputProcessor implements InputProcessor {
+	private String chatString;
+	
+	public ChatInputProcessor() {
+		chatString = "";
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		System.out.println(character);
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
