c53849992a2d551477f513cbcaaccd7660fba981
Michael Adkison <madkison3@gatech.edu>
2015-12-21 20:24:53 -0500

    implement an IndexedNode which represents a 2d position (on the map)

---
+++ core/src/com/mygdx/ai/PositionIndexedNode.java
@@ -0,0 +1,48 @@
+package com.mygdx.ai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.ai.pfa.Connection;
+import com.badlogic.gdx.ai.pfa.DefaultConnection;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedNode;
+import com.badlogic.gdx.utils.Array;
+
+public class PositionIndexedNode implements IndexedNode<PositionIndexedNode> {
+
+	public float x;
+	public float y;
+	public int index;
+	public Array<Connection<PositionIndexedNode>> connections; //a resizeable array provided by libgdx
+																//array of connections outgoing from this IndexedNode.
+	
+	public PositionIndexedNode(float x, float y, int index) {
+		this.x = x;
+		this.y = y;
+		this.index = index;
+		connections = new Array<Connection<PositionIndexedNode>>();
+	}
+
+	@Override
+	public int getIndex() {
+		return index;
+	}
+
+	@Override
+	public Array<Connection<PositionIndexedNode>> getConnections() {
+		return connections;
+	}
+	
+	public void connectToBidirectionally(PositionIndexedNode other) {
+		Connection<PositionIndexedNode> outgoingConnection = new DefaultConnection<PositionIndexedNode>(this, other);
+		this.connections.add(outgoingConnection);
+		Connection<PositionIndexedNode> incomingConnection = new DefaultConnection<PositionIndexedNode>(other, this);
+		other.connections.add(incomingConnection);
+	}
+	
+	public void connectTo(PositionIndexedNode other) {
+		Connection<PositionIndexedNode> outgoingConnection = new DefaultConnection<PositionIndexedNode>(this, other);
+		this.connections.add(outgoingConnection);
+	}
+
+}
