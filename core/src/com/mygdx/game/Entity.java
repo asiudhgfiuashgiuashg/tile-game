eaef4a71c622938dc5a6fc383730a13ecbf254b0
Mike <mikeadkison@gmail.com>
2016-05-10 19:40:02 -0400

    small fixes to ranger spritesheet

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -28,8 +28,8 @@ public abstract class Entity extends GameObject
     protected int FRAME_COLS = 13;
     protected int FRAME_ROWS = 21;
     
-    private static final float ANIMATION_DURATION = .1f;
-    private static final float SPRITE_SCALE = 3f;
+    private static final float ANIMATION_DURATION = .075f;
+    private static final float SPRITE_SCALE = 2.7f;
     SpriteBatch batch;
 
     protected Animation moveRight;
