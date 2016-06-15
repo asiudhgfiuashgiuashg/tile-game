6d3dda6af160e618d88ca4208184ee2c2106a419
Mike <mikeadkison@gmail.com>
2016-06-15 03:39:10 -0400

    add class-specific input processor stubs

---
+++ core/src/com/mygdx/game/player/class_input_processors/MageInputProcessor.java
@@ -0,0 +1,62 @@
+package com.mygdx.game.player.class_input_processors;
+
+import com.badlogic.gdx.InputProcessor;
+
+/**
+ * handle input for the mage class
+ * If the mage class is chosen by the local player, then an instance of this inputprocessor should be added to the end of the multiplexer
+ * * This processor should process class-specific inputs, not general stuff like wsad movement, which is handled by the gameinputprocessor
+ * @author elimonent
+ *
+ */
+public class MageInputProcessor implements InputProcessor {
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
